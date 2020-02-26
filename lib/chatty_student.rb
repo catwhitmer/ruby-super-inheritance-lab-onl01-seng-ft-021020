59e6c41e9613e9d01619d889ea4e23eecd786cdb
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 16:14:07 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -10,7 +10,14 @@ class ChattyStudent < Student
   def raise_hand
     super 
     super
-    
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
   end
   
 end
